adbe17e4e8f1ea81ccc9943b7ff34b35045ec789
Damien Garros <dgarros@gmail.com>
2018-10-15 12:02:36 -0700

    Update tests for ipaddr_prefix

--- tests/unit/pools/test_ipaddr_prefix.py
+++ tests/unit/pools/test_ipaddr_prefix.py
@@ -2,68 +2,125 @@ import unittest
 import sys
 import logging
 import pytest
+import ipaddress
 from os import path
 
 from resource_manager.pools.ipaddr_prefixes import PrefixesPool
 
+logging.basicConfig(level=logging.DEBUG)
 
 class Test_Validate_Init(unittest.TestCase):
-    def test_init_no_shard(self):
-        sub = PrefixesPool("192.168.0.0/16", 24)
+    def test_init_v4(self):
+        sub = PrefixesPool("192.168.0.0/28")
+        avail_subs = sub.get_nbr_available_subnets()
 
-        self.assertEqual(sub.nbr_shard, 1)
-        self.assertEqual(sub.nbr_subnets, 256)
-        self.assertEqual(list(sub.sub_by_key.keys()), ["192.168.0.0/16"])
+        self.assertEqual(avail_subs, {29: 2, 30: 0, 31: 0, 32: 0})
+        self.assertEqual(sub.available_subnets[29], ['192.168.0.0/29', '192.168.0.8/29'])
+        
+    def test_init_v6(self):
+        sub = PrefixesPool("2001:db8::0/48")
+        avail_subs = sub.get_nbr_available_subnets()
 
-    def test_init_2_shards(self):
-        sub = PrefixesPool("192.0.0.0/8", 25)
+        self.assertEqual(avail_subs[49], 2)
+        self.assertEqual(avail_subs[50], 0)
 
-        self.assertEqual(sub.nbr_shard, 2)
-        self.assertEqual(sub.nbr_subnets, 131072)
-        self.assertEqual(list(sub.sub_by_key.keys()), ["192.0.0.0/9", "192.128.0.0/9"])
+class Test_Validate_split_supernet(unittest.TestCase):
+    def test_split_first(self):
+        sub = PrefixesPool("192.168.0.0/24")
+  
+        sub.split_supernet(
+            supernet=ipaddress.ip_network('192.168.0.128/25'),
+            subnet=ipaddress.ip_network('192.168.0.128/27')
+        )
+        avail_subs = sub.get_nbr_available_subnets()
 
-    def test_init_4_shards(self):
-        sub = PrefixesPool("192.0.0.0/8", 26)
+        self.assertEqual(avail_subs[25], 1)
+        self.assertEqual(avail_subs[26], 1)
+        self.assertEqual(avail_subs[27], 2)
+        self.assertEqual(avail_subs[28], 0)
 
-        self.assertEqual(sub.nbr_shard, 4)
-        self.assertEqual(sub.nbr_subnets, 262144)
+        self.assertEqual(sub.available_subnets[25], ['192.168.0.0/25'])
+        self.assertEqual(sub.available_subnets[26], ['192.168.0.192/26'])
+        self.assertEqual(sub.available_subnets[27], ['192.168.0.128/27', '192.168.0.160/27'])
 
-    def test_init_128_shards(self):
-        sub = PrefixesPool("192.0.0.0/8", 31)
+    def test_split_middle(self):
+        sub = PrefixesPool("192.168.0.0/24")
+  
+        sub.split_supernet(
+            supernet=ipaddress.ip_network('192.168.0.128/25'),
+            subnet=ipaddress.ip_network('192.168.0.192/27')
+        )
+        avail_subs = sub.get_nbr_available_subnets()
 
-        self.assertEqual(sub.nbr_shard, 128)
-        self.assertEqual(sub.nbr_subnets, 8388608)
+        self.assertEqual(avail_subs[25], 1)
+        self.assertEqual(avail_subs[26], 1)
+        self.assertEqual(avail_subs[27], 2)
+        self.assertEqual(avail_subs[28], 0)
+
+        self.assertEqual(sub.available_subnets[25], ['192.168.0.0/25'])
+        self.assertEqual(sub.available_subnets[26], ['192.168.0.128/26'])
+        self.assertEqual(sub.available_subnets[27], ['192.168.0.192/27', '192.168.0.224/27'])
+
+    def test_split_end(self):
+        sub = PrefixesPool("192.168.0.0/16")
+  
+        sub.split_supernet(
+            supernet=ipaddress.ip_network('192.168.128.0/17'),
+            subnet=ipaddress.ip_network('192.168.255.192/27')
+        )
+        avail_subs = sub.get_nbr_available_subnets()
+
+        self.assertEqual(avail_subs[17], 1)
+        self.assertEqual(avail_subs[25], 1)
+        self.assertEqual(avail_subs[26], 1)
+        self.assertEqual(avail_subs[27], 2)
+        self.assertEqual(avail_subs[28], 0)
+
+        self.assertEqual(sub.available_subnets[17], ['192.168.0.0/17'])
+        self.assertEqual(sub.available_subnets[26], ['192.168.255.128/26'])
+        self.assertEqual(sub.available_subnets[27], ['192.168.255.192/27', '192.168.255.224/27'])
 
 
 class Test_Validate_Get_Subnet(unittest.TestCase):
     def test_v4_no_owner(self):
-        sub = PrefixesPool("192.168.0.0/16", 24)
+        sub = PrefixesPool("192.168.0.0/16")
 
-        self.assertEqual(str(sub.get_subnet()), "192.168.0.0/24")
-        self.assertEqual(str(sub.get_subnet()), "192.168.1.0/24")
+        self.assertEqual(str(sub.get_subnet(size=24)), "192.168.0.0/24")
+        self.assertEqual(str(sub.get_subnet(size=25)), "192.168.1.0/25")
+        self.assertEqual(str(sub.get_subnet(size=17)), "192.168.128.0/17")
+        self.assertEqual(str(sub.get_subnet(size=24)), "192.168.2.0/24")
+        self.assertEqual(str(sub.get_subnet(size=25)), "192.168.1.128/25")
+
+    def test_v4_with_owner(self):
+        sub = PrefixesPool("192.168.0.0/16")
+
+        self.assertEqual(str(sub.get_subnet(size=24, identifier='first')), "192.168.0.0/24")
+        self.assertEqual(str(sub.get_subnet(size=25, identifier='second')), "192.168.1.0/25")
+        self.assertEqual(str(sub.get_subnet(size=17, identifier='third')), "192.168.128.0/17")
+        self.assertEqual(str(sub.get_subnet(size=25, identifier='second')), "192.168.1.0/25")
+        self.assertEqual(str(sub.get_subnet(size=17, identifier='third')), "192.168.128.0/17")
 
     def test_no_more_subnet(self):
-        sub = PrefixesPool("192.0.0.0/22", 24)
+        sub = PrefixesPool("192.0.0.0/22")
 
-        self.assertEqual(str(sub.get_subnet()), "192.0.0.0/24")
-        self.assertEqual(str(sub.get_subnet()), "192.0.1.0/24")
-        self.assertEqual(str(sub.get_subnet()), "192.0.2.0/24")
-        self.assertEqual(str(sub.get_subnet()), "192.0.3.0/24")
-        self.assertEqual(sub.get_subnet(), False)
+        self.assertEqual(str(sub.get_subnet(size=24)), "192.0.0.0/24")
+        self.assertEqual(str(sub.get_subnet(size=24)), "192.0.1.0/24")
+        self.assertEqual(str(sub.get_subnet(size=24)), "192.0.2.0/24")
+        self.assertEqual(str(sub.get_subnet(size=24)), "192.0.3.0/24")
+        self.assertEqual(sub.get_subnet(size=24), False)
 
     @pytest.mark.long
     def test_v6_no_owner(self):
-        sub = PrefixesPool("2620:135:6000:fffe::/64", 127)
-
-        self.assertEqual(str(sub.get_subnet()), "2620:135:6000:fffe::/127")
+        sub = PrefixesPool("2620:135:6000:fffe::/64")
+        self.assertEqual(str(sub.get_subnet(size=127)), "2620:135:6000:fffe::/127")
 
 
 class Test_Validate_Check_if_Already_Reserved(unittest.TestCase):
     def test_v4_no_owner(self):
-        sub = PrefixesPool("192.168.0.0/16", 24)
+        sub = PrefixesPool("192.168.0.0/16")
 
-        self.assertEqual(str(sub.get_subnet(identifier="first")), "192.168.0.0/24")
-        self.assertEqual(str(sub.get_subnet(identifier="second")), "192.168.1.0/24")
+        self.assertEqual(str(sub.get_subnet(size=24, identifier="first")), "192.168.0.0/24")
+        self.assertEqual(str(sub.get_subnet(size=24, identifier="second")), "192.168.1.0/24")
 
         self.assertTrue(sub.check_if_already_allocated(identifier="second"))
         self.assertFalse(sub.check_if_already_allocated(identifier="third"))
@@ -71,25 +128,24 @@ class Test_Validate_Check_if_Already_Reserved(unittest.TestCase):
 
 class Test_Validate_Reserve(unittest.TestCase):
     def test_no_owner(self):
-        sub = PrefixesPool("192.168.0.0/16", 24)
+        sub = PrefixesPool("192.168.0.0/16")
 
         self.assertEqual(sub.reserve("192.168.0.0/24"), True)
-        self.assertEqual(str(sub.get_subnet()), "192.168.1.0/24")
+        self.assertEqual(str(sub.get_subnet(size=24)), "192.168.1.0/24")
 
     def test_wrong_input(self):
-        sub = PrefixesPool("192.168.0.0/16", 24)
+        sub = PrefixesPool("192.168.0.0/16")
 
         ## Out of sub
         self.assertEqual(sub.reserve("192.192.1.0/24", identifier="first"), False)
 
     def test_with_owner(self):
-        sub = PrefixesPool("192.192.0.0/16", 24)
+        sub = PrefixesPool("192.192.0.0/16")
 
         self.assertEqual(sub.reserve("192.192.0.0/24", identifier="first"), True)
         self.assertEqual(sub.reserve("192.192.1.0/24", identifier="second"), True)
 
-        self.assertEqual(str(sub.get_subnet()), "192.192.2.0/24")
-
+        self.assertEqual(str(sub.get_subnet(size=24)), "192.192.2.0/24")
 
 # # Test negative cases:
 #   - IP out of range,
